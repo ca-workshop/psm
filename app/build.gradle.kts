235e48af094c2b6358d9ebe4b861f74aa5c8440d
ca-t-swd <155301957+ca-t-swd@users.noreply.github.com>
2025-12-09 12:46:40 +0200

    Initial Android Java MVVM password manager project

---
+++ app/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.ca.passwordmanager"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.ca.unforgotten"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("androidx.navigation:navigation-fragment:2.8.8")
+    implementation("androidx.navigation:navigation-ui:2.8.8")
+
+
+    // NotificationCompat
+    implementation ("androidx.core:core-ktx:1.12.0")
+
+    // MediaSessionCompat (optional but recommended for media controls)
+    implementation ("androidx.media:media:1.6.0")
+
+    // For backward-compatible vector drawables (optional)
+    //implementation 'androidx.vectordrawable:vectordrawable:1.1.0'
+
+    // Material Design (optional, for nicer buttons etc)
+    implementation ("com.google.android.material:material:1.11.0")
+
+    //google
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+    implementation ("com.google.android.gms:play-services-maps:19.0.0")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+}
